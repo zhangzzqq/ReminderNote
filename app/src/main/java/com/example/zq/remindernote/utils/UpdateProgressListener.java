c553697d037c72206f0da202b0a4c817efa9fbb2
zhangzzqq <zhangqihappyzq@163.com>
2016-12-06 14:16:49 +0800

    提交2

---
+++ app/src/main/java/com/example/zq/remindernote/utils/UpdateProgressListener.java
@@ -0,0 +1,25 @@
+package com.example.zq.remindernote.utils;
+
+
+public interface UpdateProgressListener {
+    /**
+     * download start
+     */
+    public void start();
+
+    /**
+     * update download progress
+     * @param progress
+     */
+    public void update(int progress);
+
+    /**
+     * download success
+     */
+    public void success();
+
+    /**
+     * download error
+     */
+    public void error();
+}
